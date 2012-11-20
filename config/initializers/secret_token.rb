9c41cce5d1614a5083e0a2269a0b9b300f4e4fb1
Joel Califa <jcalifa@gmail.com>
2012-11-20 11:09:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gather::Application.config.secret_token = 'f12a9516a7943240fb480972545d3c2082b4090c4fb235b379705fd87f4c6689492206fd4d1146c9f4a44aac8d1e45f9eecef250606ab63ab615d32555b258ac'

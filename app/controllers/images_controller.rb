214cd4f14aa0dfa8c5f351ec7eec25fd91111478
Joel Califa <jcalifa@gmail.com>
2012-11-26 01:47:34 -0500

    testing no dependencies

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,18 @@
 class ImagesController < ApplicationController
+
+		def index
+			@images = Image.all
+			respond_with({:images => @images}.as_json)
+		end
+
+		def show
+			@image = Image.find(params[:id])
+			respond_with(@image)
+		end
+
+		def create
+			@image = Image.create(params[:image])
+			@tags = Tag.all
+			respond_with(@image)
+		end
 end

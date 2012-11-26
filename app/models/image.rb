1c0fed72875a41ada81996d154de64fb027d1243
Joel Califa <jcalifa@gmail.com>
2012-11-26 02:51:42 -0500

    gah

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,8 +4,8 @@ class Image < ActiveRecord::Base
   belongs_to :uploader, :class_name => "User" 
   has_and_belongs_to_many :users
 
-  has_attached_file :img,
-                    :storage => :s3,
-                    :s3_credentials => S3_CREDENTIALS
+  # has_attached_file :img,
+  #                   :storage => :s3,
+  #                   :s3_credentials => S3_CREDENTIALS
 
 end

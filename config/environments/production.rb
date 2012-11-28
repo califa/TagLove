5cdcb0f4347e9264d5903ab9fa245c803d43bf82
Joel Califa <jcalifa@gmail.com>
2012-11-27 23:47:53 -0500

    change back to credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,10 @@ Gather::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_protocol => 'http',
-    :s3_credentials => S3_CREDENTIALS
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
 end
